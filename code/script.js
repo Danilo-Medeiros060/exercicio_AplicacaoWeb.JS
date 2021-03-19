407a0a55783e5b4d27c3034572de310b95ae3260
Danilo Medeiros <danilo.medeiros060@gmail.com>
2021-03-19 11:58:49 -0300

    imagem

--- code/script.js
+++ code/script.js
@@ -1,6 +1,21 @@
-function myFunction() {
-    var selectedBGColor = document.getElementById("color").value;
-    document.body.style.backgroundColor = selectedBGColor;
+function cor() {
+    var bgcolor = document.getElementById("color").value;
+    document.body.style.backgroundColor = bgcolor;
   }
   
-document.body.style.backgroundColor = "blue";
+function img(){
+    var caixa = document.getElementById("imagens");
+    var imagem = document.getElementById("image");
+    var link = document.getElementById("link");
+
+    if (caixa.value.match("1")){
+        imagem.src = "../img/Urso.jpg";
+        link.href = "../img/Urso.jpg";
+    } if (caixa.value.match("2")) {
+        imagem.src = "../img/Foca.jpg";
+        link.href = "../img/Foca.jpg";
+    } if (caixa.value.match("3")) {
+        imagem.src = "../img/Macaco.jpg";
+    }
+}
+
