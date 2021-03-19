b6abea82c3ee3527bd6fac419e9f6fab8eabf9da
Danilo Medeiros <danilo.medeiros060@gmail.com>
2021-03-19 10:44:38 -0300

    cor

---
+++ code/script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var selectedBGColor = document.getElementById("color").value;
+    document.body.style.backgroundColor = selectedBGColor;
+  }
+  
+document.body.style.backgroundColor = "blue";
